adb87fc066a17902af50e7466081f7b24eecad57
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-07-20 22:40:31 -0600

    Back to xhr

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -1,34 +1,24 @@
 //random subreddit API request
 import Subreddit from '../components/subreddit';
 
-export const API_ROOT = 'https://www.reddit.com';
+export function getRandomSubreddit() {
 
-export const getRandomSubreddit = async () => {
-  const response = await fetch(`${API_ROOT}/r/random.json`);
-  const json = await response.json();
+  const xhr = new XMLHttpRequest();
 
-  Subreddit(json);
-};
+  const url = "https://www.reddit.com/r/random.json/";
 
-//export function getRandomSubreddit() {
+  xhr.responseType = "json";
 
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      Subreddit(xhr.response);
+    } else {
+      return "Trouble reaching the Reddit API.";
+    }
+  };
 
-//  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 
-  //const url = "https://www.reddit.com/r/random.json";
-
-//  xhr.responseType = "json";
-
-//  xhr.onreadystatechange = () => {
-//    if (xhr.readyState === XMLHttpRequest.DONE) {
-  //    Subreddit(xhr.response);
-//    } else {
-  //    return "Trouble reaching the Reddit API.";
-  //  }
-//  };
-
-//  xhr.open("GET", url);
-//  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-
-//  xhr.send();
-//}
+  xhr.send();
+}
