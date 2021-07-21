e144f9b25dff0acf12406f5d4a54869fdf9a22d2
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-07-20 23:55:58 -0600

    More testing to try and get button working

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -5,20 +5,20 @@ export function getRandomSubreddit() {
 
   const xhr = new XMLHttpRequest();
 
-  const url = "https://www.reddit.com/r/random.json/";
+  const url = "https://www.reddit.com/r/reactjs.json";
 
   xhr.responseType = "json";
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       Subreddit(xhr.response);
+      
     } else {
       return "Trouble reaching the Reddit API.";
     }
   };
 
   xhr.open("GET", url);
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 
   xhr.send();
 }
