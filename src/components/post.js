9d7cff490b1adf07ca9560efc9b608295d0e7316
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-07-22 00:16:29 -0600

    Add validator to post component as well

--- src/components/post.js
+++ src/components/post.js
@@ -5,6 +5,11 @@ import '../styles/index.css';
 
 export default function Post({post}) {
   //let randomSubreddit = JSON.stringify(post, null, 2);
+  if (!post || !post.data) {
+    return (<div className='box'>
+        <h2>Fetching results...</h2>
+        </div>)
+};
   let postData = post;
   return (<div className="box"><pre>
      {postData.data}
